2ff59d583d7028dbce3269e7cba81d021cfb574e
Badra <badra10alou10@gmail.com>
2024-07-04 15:25:28 +0000

    new version fixing...

--- depannagexpress/settings.py
+++ depannagexpress/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-u3(-)=j(gcp#y8+rp!(&3%fj$z&ox_s7ud6q5w*w@j0(ezk11-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['31.207.33.74', 'depannage-xpress.com', 'www.depannage-xpress.com', 'localhost']
 
 
 # Application definition
